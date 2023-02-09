788729423bef41b4c09d8046e44a115606547998
Eminence <eminence@rawbank.cd>
2023-02-09 09:45:37 +0100

    fixed cors access domain

--- server.js
+++ server.js
@@ -9,11 +9,18 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-
+// app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 //Routes
-app.use("/api/v1/user", cors(), require("./routes/userRoute"));
-app.use("/api/v1/customer", cors(), require("./routes/customerRoute"));
+app.use("/api/v1/user", require("./routes/userRoute"));
+app.use("/api/v1/customer", require("./routes/customerRoute"));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
